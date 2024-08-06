c39b4e6533db1b81ad1864a05149507f48c741d3
kirara <643193041@qq.com>
2024-08-06 16:28:08 +0800

    Update PickUpView.vue

--- src/views/PickUpView.vue
+++ src/views/PickUpView.vue
@@ -1,7 +1,6 @@
 <script setup>
 import {states} from '../store/index'
 import PickUp from '@/components/PickUp.vue'
-import Pin from '@/components/Pin.vue'
 import {onMounted} from 'vue'
 import {useState} from '@/store'
 import {showBackButton} from "@/helpers/helper";
